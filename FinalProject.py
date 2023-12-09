38325a61530a455532d41d2cd7f4da181905f55a
Brendan Jennings <bjennings4445@yahoo.com>
2023-12-08 21:51:43 -0600

    Added comments

--- FinalProject.py
+++ FinalProject.py
@@ -1,7 +1,9 @@
+# Import necessary modules
 import tkinter as tk
 from tkinter import ttk, messagebox, simpledialog
 import pytest
 
+# Task class represents a single task with name, description, priority, and completion status
 class Task:
     def __init__(self, name, description, priority):
         self.name = name
@@ -9,12 +11,14 @@ class Task:
         self.priority = priority
         self.completed = False
 
+# TaskManager class manages a list of tasks and their priorities
 class TaskManager:
     def __init__(self):
-        self.tasks = []
-        self.priorities = {"High": [], "Medium": [], "Low": []}
+        self.tasks = []  # List to store tasks
+        self.priorities = {"High": [], "Medium": [], "Low": []}  # Dictionary to store tasks grouped by priority
         self.load_tasks_from_file()  # Load tasks from the file during initialization
 
+    # Add a new task to the task list and update priorities
     def add_task(self, name, description, priority):
         priority = priority.capitalize()
         task = {"name": name, "description": description, "priority": priority, "completed": False}
@@ -22,17 +26,19 @@ class TaskManager:
         self.priorities[priority].append(task)
         self.save_tasks_to_file()  # Save tasks to the file after adding
 
+    # Display the list of tasks
     def view_tasks(self):
         self.load_tasks_from_file()  # Reload tasks from the file before displaying
         if not self.tasks:
             return "No tasks in the list."
-        
+
         result = "Task List:\n"
         for index, task in enumerate(self.tasks, start=1):
             status = "Done" if task["completed"] else "Not Done"
             result += f"{index}. {task['name']} - {task['description']} (Priority: {task['priority']}) - {status}\n"
         return result
 
+    # Mark a task as completed
     def mark_task_completed(self, task_index):
         if 0 < task_index <= len(self.tasks):
             self.tasks[task_index - 1]["completed"] = True
@@ -41,10 +47,7 @@ class TaskManager:
         else:
             return "Invalid task index."
 
-    def sort_tasks_by_priority(self):
-        self.tasks = sorted(self.tasks, key=lambda x: ("High", "Medium", "Low").index(x["priority"]))
-        self.save_tasks_to_file()  # Save tasks to the file after sorting
-
+    # Sort tasks based on priority using insertion sort algorithm
     def insertion_sort(self):
         for i in range(1, len(self.tasks)):
             current_task = self.tasks[i]
@@ -55,15 +58,18 @@ class TaskManager:
             self.tasks[j + 1] = current_task
         self.save_tasks_to_file()  # Save tasks to the file after insertion sort
 
+    # Get the numerical value associated with a priority
     def priority_value(self, priority):
         priority_values = {"High": 3, "Medium": 2, "Low": 1}
         return priority_values[priority]
 
+    # Save tasks to a file in a specific format
     def save_tasks_to_file(self, filename="task_list.txt"):
         with open(filename, 'w') as file:
             for task in self.tasks:
                 file.write(f"{task['name']}|{task['description']}|{task['priority']}|{task['completed']}\n")
 
+    # Load tasks from a file and update the task list and priorities
     def load_tasks_from_file(self, filename="task_list.txt"):
         self.tasks = []
         self.priorities = {"High": [], "Medium": [], "Low": []}
@@ -80,6 +86,7 @@ class TaskManager:
         except FileNotFoundError:
             pass
 
+    # Delete a task from the file based on the task index
     def delete_task_from_file(self, task_index, filename="task_list.txt"):
         if 0 < task_index <= len(self.tasks):
             deleted_task = self.tasks.pop(task_index - 1)
@@ -89,7 +96,7 @@ class TaskManager:
         else:
             return "Invalid task index."
 
-
+# TaskManagerGUI class represents the graphical user interface for the task manager
 class TaskManagerGUI:
     def __init__(self, master):
         self.master = master
@@ -98,6 +105,7 @@ class TaskManagerGUI:
         self.task_manager = TaskManager()
         self.create_widgets()
 
+    # Create and configure GUI widgets
     def create_widgets(self):
         style = ttk.Style()
         style.configure("TButton", padding=10, font=('Helvetica', 12))
@@ -124,6 +132,7 @@ class TaskManagerGUI:
         self.exit_button = ttk.Button(self.master, text="Exit", command=self.master.destroy, style="TButton")
         self.exit_button.pack(pady=10)
 
+    # Handler for adding a new task
     def add_task(self):
         name = self.get_input("Task Name:")
         description = self.get_input("Task Description:")
@@ -132,21 +141,23 @@ class TaskManagerGUI:
         if priority not in ["High", "Medium", "Low"]:
             priority = None
             messagebox.showerror("Error", "Priority must be High, Medium, or Low")
-                
+
         if name and description and priority:
             self.task_manager.add_task(name, description, priority)
             messagebox.showinfo("Success", "Task added successfully.")
         else:
             messagebox.showerror("Error", "Invalid input. Please fill in all fields.")
 
+    # Handler for viewing tasks
     def view_tasks(self):
         result = self.task_manager.view_tasks()
         messagebox.showinfo("Task List", result)
 
+    # Handler for marking a task as completed
     def mark_task_completed(self):
         result = self.task_manager.view_tasks()
         task_index = self.get_input("Enter the index of the task to mark as completed:")
-        
+
         if task_index.isdigit():
             task_index = int(task_index)
             result = self.task_manager.mark_task_completed(task_index)
@@ -154,25 +165,31 @@ class TaskManagerGUI:
         else:
             messagebox.showerror("Error", "Invalid input. Please enter a valid index.")
 
+    # Handler for sorting tasks by priority
     def sort_tasks(self):
         self.task_manager.insertion_sort()
         messagebox.showinfo("Success", "Tasks sorted by priority.")
 
+    # Handler for deleting a task
     def delete_task(self):
         result = self.task_manager.view_tasks()
         task_index = self.get_input("Enter the index of the task to delete:")
-        
+
         if task_index.isdigit():
             task_index = int(task_index)
             result = self.task_manager.delete_task_from_file(task_index)
-            messagebox.showinfo("Success", result)
+            if result == "Task deleted successfully.":
+                messagebox.showinfo("Success", result)
+            if result == "Invalid task index.":
+                messagebox.showinfo("Failure", result)
         else:
             messagebox.showerror("Error", "Invalid input. Please enter a valid index.")
 
+    # Get user input using a dialog prompt
     def get_input(self, prompt):
         return simpledialog.askstring("Input", prompt)
-    
-    # Run the GUI
+
+# Run the GUI
 root = tk.Tk()
 app = TaskManagerGUI(root)
 root.mainloop()
--- test.py
+++ test.py
@@ -1,53 +1,78 @@
+# Import necessary modules
 import pytest
 from FinalProject import TaskManager
 
+# Define a pytest fixture to create a TaskManager instance
 @pytest.fixture
 def task_manager():
     return TaskManager()
 
+# Helper function to clear the task manager by deleting all tasks
 def clear_task_manager(task_manager):
     # Iterate through tasks and delete them
     while task_manager.tasks:
         task_manager.delete_task_from_file(1)
 
+# Test case for adding a task to the TaskManager
 def test_add_task(task_manager):
+    # Add a task to the task manager
     task_manager.add_task("Task 1", "Description 1", "High")
+    # Check if the task is added successfully
     assert len(task_manager.tasks) == 1
     assert task_manager.tasks[0]["name"] == "Task 1"
+    # Clear the task manager after the test
     clear_task_manager(task_manager)
 
+# Test case for marking a task as completed in the TaskManager
 def test_mark_task_completed(task_manager):
+    # Add a task to the task manager
     task_manager.add_task("Task 1", "Description 1", "High")
+    # Mark the task as completed
     task_manager.mark_task_completed(1)
+    # Check if the task is marked as completed
     assert task_manager.tasks[0]["completed"] == True
+    # Clear the task manager after the test
     clear_task_manager(task_manager)
 
+# Test case for sorting tasks by priority in the TaskManager
 def test_sort_tasks(task_manager):
+    # Add tasks with different priorities to the task manager
     task_manager.add_task("Task 1", "Description 1", "Medium")
     task_manager.add_task("Task 2", "Description 2", "Low")
     task_manager.add_task("Task 3", "Description 3", "High")
 
+    # Check the initial order of tasks
     assert task_manager.tasks[0]["name"] == "Task 1"
     assert task_manager.tasks[1]["name"] == "Task 2"
     assert task_manager.tasks[2]["name"] == "Task 3"
 
-    task_manager.sort_tasks_by_priority()
+    # Sort tasks by priority
+    task_manager.insertion_sort()
 
+    # Check the order of tasks after sorting
     assert task_manager.tasks[0]["name"] == "Task 3"
     assert task_manager.tasks[1]["name"] == "Task 1"
     assert task_manager.tasks[2]["name"] == "Task 2"
+    # Clear the task manager after the test
     clear_task_manager(task_manager)
 
+# Test case for deleting a task from the TaskManager
 def test_delete_task(task_manager):
+    # Add tasks to the task manager
     task_manager.add_task("Task 1", "Description 1", "High")
     task_manager.add_task("Task 2", "Description 2", "Medium")
     task_manager.add_task("Task 3", "Description 3", "Low")
 
+    # Check the initial number of tasks
     assert len(task_manager.tasks) == 3
 
+    # Delete a task from the task manager
     task_manager.delete_task_from_file(2)
 
+    # Check the number of tasks after deletion
     assert len(task_manager.tasks) == 2
+    # Check the order of remaining tasks
     assert task_manager.tasks[0]["name"] == "Task 1"
     assert task_manager.tasks[1]["name"] == "Task 3"
+    # Clear the task manager after the test
     clear_task_manager(task_manager)
